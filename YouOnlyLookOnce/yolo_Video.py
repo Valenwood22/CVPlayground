2bc878c651ce1c9b35ee789ea5d07dcd760aaf81
Valenwood <joshua.gisi22@gmail.com>
2020-12-07 15:37:00 -0600

    camera support

--- YouOnlyLookOnce/yolo_Video.py
+++ YouOnlyLookOnce/yolo_Video.py
@@ -86,58 +86,178 @@ while True:
 cv2.destroyAllWindows()
 
 
-#
-# # loop through all the images
-# for img_path in images_path:
-#     # Loading image
-#     img = cv2.imread(img_path)
-#     img = cv2.resize(img, None, fx=1, fy=1)
-#     height, width, channels = img.shape
-#
-#     # Detecting objects
-#     blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
-#
-#     net.setInput(blob)
-#     outs = net.forward(output_layers)
-#
-#     # Showing information on the screen
-#     class_ids = []
-#     confidences = []
-#     boxes = []
-#     for out in outs:
-#         for detection in out:
-#             scores = detection[5:]
-#             class_id = np.argmax(scores)
-#             confidence = scores[class_id]
-#             if confidence > 0.3:
-#                 # Object detected
-#                 center_x = int(detection[0] * width)
-#                 center_y = int(detection[1] * height)
-#                 w = int(detection[2] * width)
-#                 h = int(detection[3] * height)
-#
-#                 # Rectangle coordinates
-#                 x = int(center_x - w / 2)
-#                 y = int(center_y - h / 2)
-#
-#                 boxes.append([x, y, w, h])
-#                 confidences.append(float(confidence))
-#                 class_ids.append(class_id)
-#
-#
-#     indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.3, 0.3) # defaults 0.4 0.5
-#     print(indexes)
-#     font = cv2.FONT_HERSHEY_PLAIN
-#     for i in range(len(boxes)):
-#         if i in indexes:
-#             x, y, w, h = boxes[i]
-#             label = str(classes[class_ids[i]])
-#             color = colors[class_ids[i]]
-#             cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
-#             cv2.putText(img, label, (x, y + 30), font, 3, color, 2)
-#
-#
-#     cv2.imshow("Image", img)
-#     key = cv2.waitKey(0)
-#
-# cv2.destroyAllWindows()
+# ======================================================
+
+
+import sys
+import argparse
+import subprocess
+
+import cv2
+
+
+WINDOW_NAME = 'CameraDemo'
+
+
+def parse_args():
+    # Parse input arguments
+    desc = 'Capture and display live camera video on Jetson TX2/TX1'
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('--rtsp', dest='use_rtsp',
+                        help='use IP CAM (remember to also set --uri)',
+                        action='store_true')
+    parser.add_argument('--uri', dest='rtsp_uri',
+                        help='RTSP URI, e.g. rtsp://192.168.1.64:554',
+                        default=None, type=str)
+    parser.add_argument('--latency', dest='rtsp_latency',
+                        help='latency in ms for RTSP [200]',
+                        default=200, type=int)
+    parser.add_argument('--usb', dest='use_usb',
+                        help='use USB webcam (remember to also set --vid)',
+                        action='store_true')
+    parser.add_argument('--vid', dest='video_dev',
+                        help='device # of USB webcam (/dev/video?) [1]',
+                        default=1, type=int)
+    parser.add_argument('--width', dest='image_width',
+                        help='image width [1920]',
+                        default=1920, type=int)
+    parser.add_argument('--height', dest='image_height',
+                        help='image height [1080]',
+                        default=1080, type=int)
+    args = parser.parse_args()
+    return args
+
+
+def open_cam_rtsp(uri, width, height, latency):
+    gst_str = ('rtspsrc location={} latency={} ! '
+               'rtph264depay ! h264parse ! omxh264dec ! '
+               'nvvidconv ! '
+               'video/x-raw, width=(int){}, height=(int){}, '
+               'format=(string)BGRx ! '
+               'videoconvert ! appsink').format(uri, latency, width, height)
+    return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
+
+
+def open_cam_usb(dev, width, height):
+    # We want to set width and height here, otherwise we could just do:
+    #     return cv2.VideoCapture(dev)
+    gst_str = ('v4l2src device=/dev/video{} ! '
+               'video/x-raw, width=(int){}, height=(int){} ! '
+               'videoconvert ! appsink').format(dev, width, height)
+    return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
+
+
+def open_cam_onboard(width, height):
+    gst_elements = str(subprocess.check_output('gst-inspect-1.0'))
+    if 'nvcamerasrc' in gst_elements:
+        # On versions of L4T prior to 28.1, add 'flip-method=2' into gst_str
+        gst_str = ('nvcamerasrc ! '
+                   'video/x-raw(memory:NVMM), '
+                   'width=(int)2592, height=(int)1458, '
+                   'format=(string)I420, framerate=(fraction)30/1 ! '
+                   'nvvidconv ! '
+                   'video/x-raw, width=(int){}, height=(int){}, '
+                   'format=(string)BGRx ! '
+                   'videoconvert ! appsink').format(width, height)
+    elif 'nvarguscamerasrc' in gst_elements:
+        gst_str = ('nvarguscamerasrc ! '
+                   'video/x-raw(memory:NVMM), '
+                   'width=(int)1920, height=(int)1080, '
+                   'format=(string)NV12, framerate=(fraction)30/1 ! '
+                   'nvvidconv flip-method=2 ! '
+                   'video/x-raw, width=(int){}, height=(int){}, '
+                   'format=(string)BGRx ! '
+                   'videoconvert ! appsink').format(width, height)
+    else:
+        raise RuntimeError('onboard camera source not found!')
+    return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
+
+
+def open_window(width, height):
+    cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
+    cv2.resizeWindow(WINDOW_NAME, width, height)
+    cv2.moveWindow(WINDOW_NAME, 0, 0)
+    cv2.setWindowTitle(WINDOW_NAME, 'Camera Demo for Jetson TX2/TX1')
+
+
+def read_cam(cap):
+    show_help = True
+    full_scrn = False
+    help_text = '"Esc" to Quit, "H" for Help, "F" to Toggle Fullscreen'
+    font = cv2.FONT_HERSHEY_PLAIN
+    while True:
+        if cv2.getWindowProperty(WINDOW_NAME, 0) < 0:
+            # Check to see if the user has closed the window
+            # If yes, terminate the program
+            break
+        _, img = cap.read() # grab the next image frame from camera
+        if show_help:
+            cv2.putText(img, help_text, (11, 20), font,
+                        1.0, (32, 32, 32), 4, cv2.LINE_AA)
+            cv2.putText(img, help_text, (10, 20), font,
+                        1.0, (240, 240, 240), 1, cv2.LINE_AA)
+        cv2.imshow(WINDOW_NAME, img)
+        key = cv2.waitKey(10)
+        if key == 27: # ESC key: quit program
+            break
+        elif key == ord('H') or key == ord('h'): # toggle help message
+            show_help = not show_help
+        elif key == ord('F') or key == ord('f'): # toggle fullscreen
+            full_scrn = not full_scrn
+            if full_scrn:
+                cv2.setWindowProperty(WINDOW_NAME, cv2.WND_PROP_FULLSCREEN,
+                                      cv2.WINDOW_FULLSCREEN)
+            else:
+                cv2.setWindowProperty(WINDOW_NAME, cv2.WND_PROP_FULLSCREEN,
+                                      cv2.WINDOW_NORMAL)
+
+
+def main():
+    args = parse_args()
+    print('Called with args:')
+    print(args)
+    print('OpenCV version: {}'.format(cv2.__version__))
+
+    if args.use_rtsp:
+        cap = open_cam_rtsp(args.rtsp_uri,
+                            args.image_width,
+                            args.image_height,
+                            args.rtsp_latency)
+    elif args.use_usb:
+        cap = open_cam_usb(args.video_dev,
+                           args.image_width,
+                           args.image_height)
+    else: # by default, use the Jetson onboard camera
+        cap = open_cam_onboard(args.image_width,
+                               args.image_height)
+
+    if not cap.isOpened():
+        sys.exit('Failed to open camera!')
+
+    open_window(args.image_width, args.image_height)
+    read_cam(cap)
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
