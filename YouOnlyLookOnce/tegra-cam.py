216a24db159c68257ecf5cd1037b4ca4e36fa16b
Valenwood <joshua.gisi22@gmail.com>
2020-12-07 22:32:22 -0600

    works but is slow

--- YouOnlyLookOnce/tegra-cam.py
+++ YouOnlyLookOnce/tegra-cam.py
@@ -1,19 +1,24 @@
-__authors__ = ["Joshua Gisi"]
-__copyright__ = ""
-__version__ = "Experimental"
-__email__ = "Joshua.Gisi@ndus.edu"
-__status__ = "Development"
+# --------------------------------------------------------
+# Camera sample code for Tegra X2/X1
+#
+# This program could capture and display video from
+# IP CAM, USB webcam, or the Tegra onboard camera.
+# Refer to the following blog post for how to set up
+# and run the code:
+#   https://jkjung-avt.github.io/tx2-camera-with-python/
+#
+# Written by JK Jung <jkjung13@gmail.com>
+# --------------------------------------------------------
 
 
-import cv2
-import numpy as np
-import glob
-import random
 import sys
 import argparse
 import subprocess
 
-WINDOW_NAME = 'SD2016 Hand Gesture Recognition'
+import cv2
+
+
+WINDOW_NAME = 'CameraDemo'
 
 
 def parse_args():
@@ -45,7 +50,6 @@ def parse_args():
     return args
 
 
-
 def open_cam_rtsp(uri, width, height, latency):
     gst_str = ('rtspsrc location={} latency={} ! '
                'rtph264depay ! h264parse ! omxh264dec ! '
@@ -56,7 +60,6 @@ def open_cam_rtsp(uri, width, height, latency):
     return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
 
 
-
 def open_cam_usb(dev, width, height):
     # We want to set width and height here, otherwise we could just do:
     #     return cv2.VideoCapture(dev)
@@ -64,7 +67,6 @@ def open_cam_usb(dev, width, height):
                'video/x-raw, width=(int){}, height=(int){} ! '
                'videoconvert ! appsink').format(dev, width, height)
     return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
-
 
 
 def open_cam_onboard(width, height):
@@ -93,13 +95,11 @@ def open_cam_onboard(width, height):
     return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
 
 
-
 def open_window(width, height):
     cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
     cv2.resizeWindow(WINDOW_NAME, width, height)
     cv2.moveWindow(WINDOW_NAME, 0, 0)
-    cv2.setWindowTitle(WINDOW_NAME, 'SD2016')
-
+    cv2.setWindowTitle(WINDOW_NAME, 'Camera Demo for Jetson TX2/TX1')
 
 
 def read_cam(cap):
@@ -133,141 +133,35 @@ def read_cam(cap):
                 cv2.setWindowProperty(WINDOW_NAME, cv2.WND_PROP_FULLSCREEN,
                                       cv2.WINDOW_NORMAL)
 
-if __name__ == '__main__':
 
+def main():
     args = parse_args()
     print('Called with args:')
     print(args)
     print('OpenCV version: {}'.format(cv2.__version__))
 
-    # Load Yolo
-    net = cv2.dnn.readNet("/home/sd2016/cvapp/CVPlayground/YouOnlyLookOnce/yolo-obj_last.weights", "/home/sd2016/cvapp/CVPlayground/YouOnlyLookOnce/yolo-obj.cfg")
-
-    # Name custom object
-    classes = ["RH_Zero","RH_One","RH_Two","RH_Three","RH_Four","RH_Five",]
-
-    layer_names = net.getLayerNames()
-    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-    colors = np.random.uniform(0, 255, size=(len(classes), 3))
-
-    # cap = cv2.VideoCapture(0)
-    cap = open_cam_onboard(args.image_width, args.image_height)
-    # cap = open_cam_usb(args.video_dev, args.image_width, args.image_height)
-    # cap.set(3, 1280) # Screen size
-    # cap.set(4,1024) # Screen size
-    # cap.set(10,100) # Brightness
+    if args.use_rtsp:
+        cap = open_cam_rtsp(args.rtsp_uri,
+                            args.image_width,
+                            args.image_height,
+                            args.rtsp_latency)
+    elif args.use_usb:
+        cap = open_cam_usb(args.video_dev,
+                           args.image_width,
+                           args.image_height)
+    else: # by default, use the Jetson onboard camera
+        cap = open_cam_onboard(args.image_width,
+                               args.image_height)
 
     if not cap.isOpened():
         sys.exit('Failed to open camera!')
 
     open_window(args.image_width, args.image_height)
-    
-    predictionCounter = 0
-
-    while True:
-        success, img = cap.read()
-        
-        if predictionCounter >= 0:
-            height, width, channels = img.shape
-
-            blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
-            net.setInput(blob)
-            outs = net.forward(output_layers)
-
-            # Showing information on the screen
-            class_ids = []
-            confidences = []
-            boxes = []
-            for out in outs:
-                for detection in out:
-                    scores = detection[5:]
-                    class_id = np.argmax(scores)
-                    confidence = scores[class_id]
-                    if confidence > 0.3:
-                        # Object detected
-                        center_x = int(detection[0] * width)
-                        center_y = int(detection[1] * height)
-                        w = int(detection[2] * width)
-                        h = int(detection[3] * height)
-
-                        # Rectangle coordinates
-                        x = int(center_x - w / 2)
-                        y = int(center_y - h / 2)
-
-                        boxes.append([x, y, w, h])
-                        confidences.append(float(confidence))
-                        class_ids.append(class_id)
-
-
-            indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.3, 0.3) # defaults 0.4 0.5
-            print(indexes)
-            font = cv2.FONT_HERSHEY_PLAIN
-            for i in range(len(boxes)):
-                if i in indexes:
-                    x, y, w, h = boxes[i]
-                    label = str(classes[class_ids[i]])
-                    color = colors[class_ids[i]]
-                    cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
-                    cv2.putText(img, label, (x, y + 30), font, 3, color, 2)
-            predictionCounter = 0
-        else:
-            predictionCounter += 1
-
-
-        cv2.imshow(WINDOW_NAME, img)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    read_cam(cap)
 
     cap.release()
     cv2.destroyAllWindows()
 
 
-# =======================================================================================================
-#
-#
-# import sys
-# import argparse
-# import subprocess
-#
-# import cv2
-#
-#
-# WINDOW_NAME = 'CameraDemo'
-#
-# def main():
-#     args = parse_args()
-#     print('Called with args:')
-#     print(args)
-#     print('OpenCV version: {}'.format(cv2.__version__))
-#
-#     if args.use_rtsp:
-#         cap = open_cam_rtsp(args.rtsp_uri,
-#                             args.image_width,
-#                             args.image_height,
-#                             args.rtsp_latency)
-#     elif args.use_usb:
-#         cap = open_cam_usb(args.video_dev,
-#                            args.image_width,
-#                            args.image_height)
-#     else: # by default, use the Jetson onboard camera
-#         cap = open_cam_onboard(args.image_width,
-#                                args.image_height)
-#
-#     if not cap.isOpened():
-#         sys.exit('Failed to open camera!')
-#
-#     open_window(args.image_width, args.image_height)
-#     read_cam(cap)
-#
-#     cap.release()
-#     cv2.destroyAllWindows()
-#
-#
-# if __name__ == '__main__':
-#     main()
-
-
-
-
-
-
+if __name__ == '__main__':
+    main()
